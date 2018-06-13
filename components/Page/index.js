ea55713b5c2e23cfcc4fb464bd57abceebaf2a68
evgeniy-bond <kodu34@mail.ru>
2018-06-14 00:50:47 +0300

    styles

--- components/Page/index.js
+++ components/Page/index.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import { View, Text, Image } from 'react-native';
+import s from './styles';
 
 export default class Page extends Component {
   render() {
     const { title, children } = this.props;
     
     return (
-      <View style={{ paddingTop: 20, paddingBottom: 20 }}>
-        <Text style={{ fontSize: 24, textAlign: 'center' }}>{title}</Text>
+      <View style={s.container}>
+        <Text style={s.text}>{title}</Text>
         {children}
       </View>
     );
---
+++ components/Page/styles.js
@@ -0,0 +1,12 @@
+export default {
+  container: {
+    paddingTop: 20, 
+    paddingBottom: 20 
+  },
+  text: { 
+    fontSize: 24, 
+    textAlign: 'center', 
+    paddingBottom: 5,
+    paddingTop: 5
+  }
+}
